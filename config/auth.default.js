20a4b32c0fc11f2a94cd4bc796be94776d140e67
Cataldo Malerba <cataldo.malerba@regesta.local>
2020-05-12 22:31:59 +0200

    Add default config files

---
+++ config/auth.default.js
@@ -0,0 +1,14 @@
+var googleOAuth2 = {
+  clientID: "",
+  clientSecret: "",
+};
+
+var facebookAuth = {
+  clientID: "",
+  clientSecret : ""
+}
+
+module.exports = {
+    googleOAuth2 : googleOAuth2,
+    facebook : facebookAuth
+};
